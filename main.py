f777259f66b976cb9ee2e87f484a540bbf1e17f5
Derpez <85128296+Derpez@users.noreply.github.com>
2022-03-09 17:42:15 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Greet And Name
+name = "Derpez"
+greet = "Hello, How Are You"
+
+# Response 1 and 2
+response1 = "Great, Me Too"
+response2 = "Cool!"
+
+print(greet + " " + name + "?")
+
+input()
+
+print(response1)
+print("What Are You Doing Right Now?")
+
+input()
+
+print(response2)
+print("Alright Well I Gotta Get Going. Bye and See You Soon!")
+
+Final = input("Type Done To Close: ")
+
+if Final == "Done":
+    SystemExit
